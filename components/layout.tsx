76d1b810bc1ef06909503b1e41b6091c6ca78fa0
Liam Hogan <hoganliam3@gmail.com>
2023-05-18 14:59:57 -0600

    changed sample website

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Liam Hogan'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = "Liam Hogan's Blog"
 
 export default function Layout({
   children,
